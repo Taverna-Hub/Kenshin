ca4df1b8c9c521d4b9900defc8c81750c4203ff6
n3waz <lhk@cesar.school>
2024-05-07 11:09:05 -0300

    first try to make something move

--- main.c
+++ main.c
@@ -1,87 +1,79 @@
-/**
- * main.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
-#include <string.h>
-
+// #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
 
-int x = 34, y = 12;
-int incX = 1, incY = 1;
-
-void printHello(int nextX, int nextY)
-{
-    screenSetColor(CYAN, DARKGRAY);
-    screenGotoxy(x, y);
-    printf("           ");
-    x = nextX;
-    y = nextY;
-    screenGotoxy(x, y);
-    printf("Hello World");
-}
-
-void printKey(int ch)
-{
-    screenSetColor(YELLOW, DARKGRAY);
-    screenGotoxy(35, 22);
-    printf("Key code :");
-
-    screenGotoxy(34, 23);
-    printf("            ");
-    
-    if (ch == 27) screenGotoxy(36, 23);
-    else screenGotoxy(39, 23);
-
-    printf("%d ", ch);
-    while (keyhit())
-    {
-        printf("%d ", readch());
-    }
-}
-
-int main() 
-{
+int main() {
     static int ch = 0;
+    int player1X = 34, player1Y = 12;
+    int player2X = 34, player2Y = 12;
+    int inc1X = 0, inc1Y = 0;
+    int inc2X = 0, inc2Y = 0;
+    int player1Health = 100;
+    int player2Health = 100;
 
     screenInit(1);
     keyboardInit();
     timerInit(50);
 
-    printHello(x, y);
-    screenUpdate();
-
-    while (ch != 10) //enter
-    {
+    while (ch != 10) { // enter
         // Handle user input
-        if (keyhit()) 
-        {
+        if (keyhit()) { // funcao da keyboard (ñ indica hit)
             ch = readch();
-            printKey(ch);
-            screenUpdate();
+            if (ch == 27) { // escape
+                screenDestroy();
+                keyboardDestroy();
+                timerDestroy();
+                return 0;
+            } else if (ch == 97) { // a
+                inc1X = -1;
+            } else if (ch == 119) { // w
+                inc1Y = -1;
+            } else if (ch == 115) { // s
+                inc1Y = +1;
+            } else if (ch == 100) { // d
+                inc1X = +1;
+            } else if (ch == 113) { // q
+                // player 1 attack
+                player2Health -= 10;
+                printf("Player 1 attacks Player 2 for 10 damage.\n");
+            } else if (ch == 117) { // u
+                // player 2 attack
+                player1Health -= 10;
+                printf("Player 2 attacks Player 1 for 10 damage.\n");
+            }
         }
 
         // Update game state (move elements, verify collision, etc)
-        if (timerTimeOver() == 1)
-        {
-            int newX = x + incX;
-            if (newX >= (MAXX -strlen("Hello World") -1) || newX <= MINX+1) incX = -incX;
-            int newY = y + incY;
-            if (newY >= MAXY-1 || newY <= MINY+1) incY = -incY;
+        if (timerTimeOver() == 1) {
+            player1X += inc1X;
+            player1Y += inc1Y;
+            player2X += inc2X;
+            player2Y += inc2Y;
 
-            printKey(ch);
-            printHello(newX, newY);
-
+            screenGotoxy(player1X, player1Y);
+            printf("@");
+            screenGotoxy(player2X, player2Y);
+            printf("@");
             screenUpdate();
+
+            printf("Player 1 Health: %d\n", player1Health);
+            printf("Player 2 Health: %d\n", player2Health);
+
+            if (player1Health <= 0) {
+                printf("Player 1 has died. Game over.\n");
+                break;
+            } else if (player2Health <= 0) {
+                printf("Player 2 has died. Game over.\n");
+                break;
+            }
         }
     }
 
-    keyboardDestroy();
     screenDestroy();
+    keyboardDestroy();
     timerDestroy();
 
     return 0;

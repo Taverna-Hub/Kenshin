feae197ddc088a5766a3e85a2b095d88f4382b2b
viniciusdandrade <vaj@cesar.school>
2024-05-26 20:03:50 -0300

    starting to create menus

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
+#include "./ascii.h"
 
 #define SPRITE_WIDTH 7
 #define SPRITE_HEIGHT 4
@@ -15,7 +16,7 @@
 #define DF_STATE 2
 #define JUMP_STATE 3
 #define MAX_JUMP_HEIGHT 6
-#define ATK_CLWDN 130
+#define ATK_CLWDN 80
 
 #define ALTURA_BAMBOO 10
 #define QTD_BAMBOO 8
@@ -28,6 +29,9 @@ int player2Jumping = 0;
 int alturaTela = 25;
 int player1LastAttackTime = 0;
 int player2LastAttackTime = 0;
+int menuHOF = 0;
+int menuInstructions = 0;
+
 void printSprite(int x, int y, char sprite[SPRITE_HEIGHT][SPRITE_WIDTH + 1])
 {
     for (int i = 0; i < SPRITE_HEIGHT; i++)
@@ -146,7 +150,7 @@ void grassFloor(int x, int y)
 }
 void updateSprites(int player1X, int player2X, char (**sprite1)[SPRITE_HEIGHT][SPRITE_WIDTH + 1], char (**sprite2)[SPRITE_HEIGHT][SPRITE_WIDTH + 1], int player1State, int player2State)
 {
-    if (player1X < player2X)
+    if (player1X-2 < player2X)
     {
         *sprite1 = (player1State == BASE_STATE) ? &baseSprite1 : (player1State == ATK_STATE) ? &attackSprite1 : &defenseSprite1;
         *sprite2 = (player2State == BASE_STATE) ? &baseSprite2 : (player2State == ATK_STATE) ? &attackSprite2 : &defenseSprite2;
@@ -308,7 +312,83 @@ int main()
     int maxHealth = 100;
     char (*player1Sprite)[SPRITE_HEIGHT][SPRITE_WIDTH + 1] = &baseSprite1;
     char (*player2Sprite)[SPRITE_HEIGHT][SPRITE_WIDTH + 1] = &baseSprite2;
+    screenInit(0);
+    
+    screenHideCursor();
 
+    keyboardInit();
+
+    system("clear");
+    screenSetColor(RED, DARKGRAY);
+    printf("%s\n", logo);
+
+    printf("%s\n\n\n", kenshin);
+
+    screenSetColor(LIGHTRED, DARKGRAY);
+    printf("%s\n\n", start);
+    
+    screenSetColor(LIGHTRED, DARKGRAY);
+    printf("%s\n", instructions);
+
+    while (ch != 32) {
+        if (keyhit())
+        {
+            ch = readch();
+        }
+        if (ch == 73 || ch == 105) {
+            menuInstructions = 1;
+            break;
+        }
+        if(ch == 76 || ch == 108) {
+            menuHOF = 1;
+            break;
+        }
+    }
+     if (menuInstructions) {
+        screenInit(0);
+        screenSetColor(RED, DARKGRAY);
+    
+
+        screenSetColor(LIGHTRED, DARKGRAY);
+        screenGotoxy(30, 6);
+        printf("First player's keys");
+        screenGotoxy(30, 8);
+        printf("Press W to jump: ");
+        printSprite(34, 10, baseSprite1);
+        screenGotoxy(30, 16);
+        printf("Press Q to attack: ");
+        printSprite(34, 18, attackSprite1);
+        screenGotoxy(30, 26);
+        printf("Press E to defend: ");
+        printSprite(34, 28, defenseSprite1);
+        screenGotoxy(80, 6);
+        printf("Second player's keys");
+        screenGotoxy(80, 8);
+        printf("Press I to jump: ");
+        printSprite(84, 10, baseSprite2);
+        screenGotoxy(80, 16);
+        printf("Press U to attack: ");
+        printSprite(84, 18, attackSprite2);
+        screenGotoxy(80, 26);
+        printf("Press O to defend: ");
+        printSprite(84, 28, defenseSprite2);
+        
+        screenGotoxy(40, 34);
+        printf("First player's lateral movement: A and D.");
+        screenGotoxy(40, 35);
+        printf("Second player's lateral movement: J and L.");
+        screenGotoxy(40, 36);
+        printf("Deplete your enemy's life to win.");
+
+        
+        printf("\n\n          %s\n\n", start);
+        while (ch != 32) {
+            if (keyhit()) {
+                ch = readch();
+            }
+        }
+    }
+    screenSetColor(WHITE, DARKGRAY);
     screenInit(1);
     keyboardInit();
     timerInit(180);
@@ -344,7 +424,7 @@ int main()
                     player1LastAttackTime = currentTime;
                     player1State = ATK_STATE;
                     updatePlayer(&player1X, &player1Y, 0, 0, *player1Sprite, player2X, player2Y, *player2Sprite, player2Jumping);
-                    if (player1X + SPRITE_WIDTH == player2X + 1 || player1X == player2X + SPRITE_WIDTH - 1)
+                    if (player1X + SPRITE_WIDTH == player2X +2  || player1X == player2X + SPRITE_WIDTH - 2)
                     {
                         player2Health -= (player2State == DF_STATE) ? DF_DMG : ATK_DMG;
                     }
@@ -370,7 +450,7 @@ int main()
                     player2LastAttackTime = currentTime;
                     player2State = ATK_STATE;
                     updatePlayer(&player2X, &player2Y, 0, 0, *player2Sprite, player1X, player1Y, *player1Sprite, player1Jumping);
-                    if (player2X == player1X + SPRITE_WIDTH - 1 || player2X + SPRITE_WIDTH == player1X + 1)
+                    if (player2X == player1X + SPRITE_WIDTH - 2 || player2X + SPRITE_WIDTH == player1X + 2)
                     {
                         player1Health -= (player1State == DF_STATE) ? DF_DMG : ATK_DMG;
                     }
@@ -409,45 +489,45 @@ int main()
             // drawBamboo(50, alturaTela - 4, ALTURA_BAMBOO, QTD_BAMBOO);
             drawBackground(2, alturaTela - 18);
             // drawHouse(25, alturaTela - 10);
-            drawHealthBar(10, 5, player1Health, maxHealth);
-            drawHealthBar(66, 5, player2Health, maxHealth);
+            drawHealthBar(10, 3, player1Health, maxHealth);
+            drawHealthBar(66, 3, player2Health, maxHealth);
 
             handleJump(&player1X, &player1Y, &player1JumpHeight, &player1Jumping, &player1State, baseSprite1);
             handleJump(&player2X, &player2Y, &player2JumpHeight, &player2Jumping, &player2State, baseSprite2);
             if (player1Jumping && player1Y + SPRITE_HEIGHT > player2Y && player1X + SPRITE_WIDTH-8 > player2X && player1X < player2X + SPRITE_WIDTH)
             {
                 clearSprite(player1X, player1Y, SPRITE_WIDTH, SPRITE_HEIGHT);
-                player1X = player2X + SPRITE_WIDTH;
+                player1X = player2X + SPRITE_WIDTH-2;
             }
             else if(player1Jumping && player1Y + SPRITE_HEIGHT > player2Y && player1X + SPRITE_WIDTH >2+ player2X && player1X < player2X + SPRITE_WIDTH){
                 clearSprite(player1X, player1Y, SPRITE_WIDTH, SPRITE_HEIGHT);
-                player1X = player2X - SPRITE_WIDTH+1;
+                player1X = player2X - SPRITE_WIDTH+2;
             }
             if (player2Jumping && player2Y + SPRITE_HEIGHT > player1Y && player2X + SPRITE_WIDTH > player1X && player2X +8< player1X + SPRITE_WIDTH)
             {
                 clearSprite(player2X, player2Y, SPRITE_WIDTH, SPRITE_HEIGHT);
-                player2X = player1X - SPRITE_WIDTH;
+                player2X = player1X - SPRITE_WIDTH+2;
             }
             else if (player2Jumping && player2Y + SPRITE_HEIGHT > player1Y && player2X +4 >= player1X && player2X < player1X + SPRITE_WIDTH)
             {
                 clearSprite(player2X, player2Y, SPRITE_WIDTH, SPRITE_HEIGHT);
-                player2X = player1X + SPRITE_WIDTH-1;
+                player2X = player1X + SPRITE_WIDTH-2;
             }
             
             updateSprites(player1X, player2X, &player1Sprite, &player2Sprite, player1State, player2State);
             if (player1State == ATK_STATE){
-            printSprite(player2X,player2Y,*player2Sprite);
-            printSprite(player1X,player1Y,*player1Sprite);
+                printSprite(player2X,player2Y,*player2Sprite);
+                printSprite(player1X,player1Y,*player1Sprite);
 
             }
             else if (player2State==ATK_STATE){
                  printSprite(player1X,player1Y,*player1Sprite);
-            printSprite(player2X,player2Y,*player2Sprite);
+                printSprite(player2X,player2Y,*player2Sprite);
 
             }
             else{
                  printSprite(player1X,player1Y,*player1Sprite);
-            printSprite(player2X,player2Y,*player2Sprite);
+                printSprite(player2X,player2Y,*player2Sprite);
             }
             if (player1Health <= 0)
             {
